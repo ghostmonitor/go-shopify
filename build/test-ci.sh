24b70f6af263fbf74b0a7dbc131b96d00d07bb4b
recart-platform-internal <ops@recart.com>
2023-06-14 13:34:16 +0200

    :wrench: updated the test ci file [skip ci] Created automatically by ghostmonitor/github-config

---
+++ build/test-ci.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+REGISTRY_ID="${AWS_ECR_REGISTRY_ID:?AWS_ECR_REGISTRY_ID env var is required}"
+REGION=${AWS_DEFAULT_REGION:-"us-east-1"}
+if [ "${CI}" = "true" ]; then
+AWS_ECR_LOGIN="${AWS_ECR_LOGIN:-0}"
+else
+AWS_ECR_LOGIN="${AWS_ECR_LOGIN:-1}"
+fi
+
+if [ "${AWS_ECR_LOGIN}" = "1" ]; then 
+	aws ecr get-login-password --region "${REGION}" | docker login --username AWS --password-stdin "${REGISTRY_ID}.dkr.ecr.${REGION}.amazonaws.com"
+fi
+DOCKER_BUILDKIT=0 docker compose up --abort-on-container-exit --build
+
